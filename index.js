a0b4db345e007c806f60f15b58d9ea51eb3ae992
wendellemes <wendellemes0@gmail.com>
2021-09-27 08:54:35 -0400

    atualizado

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+const port = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
 const path = require('path');
  
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname , "/teste.html"));
+  res.sendFile(path.join(__dirname , "/calculadora.html"));
 });
-app.listen(8080);
-
+app.listen(port);
